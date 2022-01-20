1f25153ca20f6392769b523168dd2c4d26b33f0f
Qim Lönnegren <qim.lonnegren@iths.se>
2022-01-20 13:21:18 +0100

    Create components and views

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  
+  <div id="app"></div>
 </template>
 
 <script>
 export default {
-
-}
+  data(){return{
+  }}
+};
 </script>
 
 <style>
-
+#app {
+  font-family: 'Courier New', Courier, monospace;
+  text-align: center;
+  color: black;
+  margin-top: 50px;
+}
 </style>
